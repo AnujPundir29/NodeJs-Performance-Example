b0c19dd0138933fa5e8c285a69bfdfe43b9faa1a
AnujPundir29 <pundiranuj28@gmail.com>
2021-11-02 02:39:00 +0530

    making cluster

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cluster = require('cluster');
 
 const app = express();
 
@@ -10,13 +11,25 @@ function delay(duration) {
 }
 
 app.get('/', (req, res) => {
-    res.send('Performance example');
+    res.send(`Performance example ${process.pid}`);
 })
 
 app.get('/timers', (req, res) => {
     //delay the response    
     delay(9000);
-    res.send('ding ding!');
+    res.send(`ding ding! ${process.pid}`);
 })
 
-app.listen(3000, () => console.log('Listening to 3000....'));
+if(cluster.isMaster)
+{
+    console.log('Master right now!!!');
+    //mAking child process 
+    cluster.fork();
+    cluster.fork();
+}
+else
+{
+    console.log('Right now in worker');
+    app.listen(3000, () => console.log('Listening to 3000....'));
+}
+
