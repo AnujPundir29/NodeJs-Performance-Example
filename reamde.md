d2dc15dba8881528b5e1abb83f70a76d63bcbc08
AnujPundir29 <pundiranuj28@gmail.com>
2021-11-05 17:22:58 +0530

    pm2 uses

---
+++ reamde.md
@@ -0,0 +1,8 @@
+->  pm2 commands
+
+1.  pm2 start server.js -i max
+2.  pm2 stop server
+3.  pm2 delete server
+4.  pm2 reload server
+
+
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
-const cluster = require('cluster');
-const os = require('os');
+// const cluster = require('cluster');
+// const os = require('os');
 
 const app = express();
 
@@ -18,19 +18,20 @@ app.get('/', (req, res) => {
 app.get('/timers', (req, res) => {
     //delay the response    
     delay(9000);
+    // zero downtime reload -> pm2 reload sever
     res.send(`ding ding! ${process.pid}`);
 })
 
-if (cluster.isMaster) {
-    console.log('Master right now!!!');
+// if (cluster.isMaster) {
+//     console.log('Master right now!!!');
     //mAking child process 
-    cluster.fork();
-    cluster.fork();
+    // cluster.fork();
+    // cluster.fork();
 
     // const Num_workers = os.cpus().length;
     // for (let i = 0; i < Num_workers; i++)
     //     cluster.fork();
-} else {
+// } else {
     console.log('Right now in worker');
     app.listen(3000, () => console.log('Listening to 3000....'));
-}
+// }
